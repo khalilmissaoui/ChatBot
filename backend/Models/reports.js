8b15fb1e96127b0b9f253fd9718398e74721a132
khalil <khalil.missaoui1@esprit.tn>
2021-05-01 18:34:30 +0100

    message3

---
+++ backend/Models/reports.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/PI')
+.then( ()=>{console.log('connected to MongoDB...')} )
+.catch( (er)=> console.log(er) )
+
+const courseSchenma = new mongoose.Schema({
+    name: String, 
+    content : String ,
+    state : {
+        type: String,
+        enum : ['Not_fixed','fixed','Pending'],
+        default: 'Not_fixed'
+    },
+    send_date : { type : Date , "default" : Date.now },
+    sender:
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "user"
+        },
+    img : String
+})  
+
+const report = mongoose.model('report',courseSchenma)
+
+exports.Report = report;
---
+++ frontend/src/components/AdminOffice.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { UserContext } from '../contextProvider/contextProvider';
+import { useHistory } from "react-router-dom";
+import { TheLayout } from '../containers';
+import Dashboard from '../views/dashboard/Dashboard';
+
+function AdminOffice() {
+   
+   // const history = useHistory();
+
+    // React.useEffect(() => {
+        // console.log('admin user',user);
+        // if(user.role=='user'){
+        //     console.log('pushedd ');
+        //     history.push('/')
+        // }
+
+    // }, [])
+
+    return (
+        <div>
+            admin
+        </div>
+    )
+}
+
+export default AdminOffice
