24ad133f3dcd35d993bed2e2755f50d7f004830b
khalil <75956233+ChedyRh@users.noreply.github.com>
2021-05-10 04:06:32 +0100

    make it better

--- frontend/src/axios/axios.js
+++ frontend/src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL:"http://localhost:3001/"
+    baseURL:"https://chatbotpi.herokuapp.com/"
 })
 
 //https://chatbotpi.herokuapp.com/
